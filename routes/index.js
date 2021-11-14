fb801d24892b568c02442265355ae26b0dfc74f5
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2021-11-13 19:27:49 -0500

    Deleted unused items

--- routes/index.js
+++ routes/index.js
@@ -23,21 +23,4 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// router.get('/surveys/edit/:id', surveyController.displayEditPage);
-
-// router.get('/surveys/survey/:id', surveyController.close_up);
-
-// router.post('/surveys/edit/:id', surveyController.processEditPage);
-
-// router.get('/surveys/addquestion/:id', surveyController.displayQuestionPage);
-
-// router.post('/surveys/addquestion/:id', surveyController.processQuestionPage);
-
-// router.get('/surveys/delete/:id', surveyController.performDelete);
-
-// router.get('/surveys/add', surveyController.displayAddPage);
-
-// router.post('/surveys/add', surveyController.processAddPage);
-// router.get('/surveys/list', surveyController.surveys)
-
 module.exports = router;
