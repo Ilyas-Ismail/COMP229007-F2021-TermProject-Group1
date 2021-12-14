3f7a0754caafbc460c56e9dbddfdf2c1ac4974be
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2021-12-13 23:32:27 -0500

    Removed references to client from app.js

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@
 // Date: 11/10/2021 (November 11th, 2021)
 
 module.exports = {
-    // "URI": "mongodb+srv://admin:KPxwj6d89gGDedy7@termmongodb.bmpv8.mongodb.net/surveys?retryWrites=true&w=majority",
-    // "secret": "$C&F)J@McQfTjWnZr4u7x!A%D*G-KaPdRgUkXp2s5v8y/B?E(H+MbQeThVmYq3t6"
-    "URI": process.env.ATLASDB,
-    "secret" : process.env.SECRETKEY
+    "URI": "mongodb+srv://admin:KPxwj6d89gGDedy7@termmongodb.bmpv8.mongodb.net/surveys?retryWrites=true&w=majority",
+    "secret": "$C&F)J@McQfTjWnZr4u7x!A%D*G-KaPdRgUkXp2s5v8y/B?E(H+MbQeThVmYq3t6"
+    // "URI": process.env.ATLASDB,
+    // "secret" : process.env.SECRETKEY
 
 
 }
